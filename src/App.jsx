5cddf6ea6d302c7c2edfe492b2107bf0e362032f
Roman <ramon1997.n8@gmail.com>
2022-05-19 20:48:46 +0300

    Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,105 @@
-import { useState } from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import ButtonColor from './components/ColorButton';
-import GetPassword from './components/GetPassword';
-
-
-
-
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState()
+  const [discription, setDiscription] = useState()
+  const [price, setPrice] = useState()
+  const [type, setType] = useState([])
+
+  const [useChecked, setChecked] = useState(!'checked')
+
+  function whichIsTrue(target) {
+    useChecked === !'checked' ? setChecked('checked') : setChecked(!'checked')
+      if(useChecked === 'checked'){
+        setType(target)
+      }else{
+         setType('')
+     }
+  }
+
+  function show() {
+    console.log(
+      ` name: ${name};
+  discription: ${discription};
+  price: ${price};
+  type: ${type}
+      `
+    )
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <ButtonColor />
-        <GetPassword />
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className='center'>
+      <div>
+        <input value={name} placeholder='name'
+          onChange={(e) => {
+            setName(e.target.value)
+          }}
+          style={{
+            width:'200px',
+            height:'20px',
+            marginTop: "50px"
+          }}
+        />
+      </div>
+      <div>
+        <input value={discription} placeholder='discription'
+          onChange={(e) => {
+            setDiscription(e.target.value)
+          }}
+          style={{
+            width:'200px',
+            height:'20px',
+            marginTop: "10px"
+          }}
+        />
+      </div>
+      <div>
+      <select className='center' onChange={(e) => {
+            setPrice(e.target.value)
+          }}
+          style={{
+            width:'210px',
+            height:'25px',
+            marginTop: "10px"
+          }}
+      >
+      <option value='price'>price</option>
+      <option value='100'>100</option>
+      <option value='200'>200</option>
+      <option value='300'>300</option>
+      <option value='400'>400</option>
+      <option value='500'>500</option>
+      </select>
+      </div>
+
+        <div style={{margin: 10}}>
+          <label>
+            <input type='checkbox' checked={useChecked} value='tshirt' onChange={(e) => {
+              whichIsTrue(e.target.value)
+            }}/>
+          tshirt</label>
+
+          <label>
+            <input type='checkbox' checked={useChecked} value='jeans' onChange={(e) => {
+              whichIsTrue(e.target.value)
+            }}/>
+          jeans</label>
+
+          <label>
+            <input type='checkbox' checked={useChecked} value='sweatshirt' onChange={(e) => {
+              whichIsTrue(e.target.value)
+            }}/>
+          sweatshirt</label>
+        </div>
+
+        <button type='submit' onClick={() => {show(name, discription, price, type)}} style={{
+            width:'200px',
+            height:'30px',
+            marginTop: "15px",
+            fontSize: '12px',
+            background: "#B0C4DE"
+          }}>create</button>
     </div>
-  );
+  )
 }
