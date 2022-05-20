9ed545390d3c0853d0169d8b7cb119109b2816d0
Roman <ramon1997.n8@gmail.com>
2022-05-20 19:43:50 +0300

    Hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,16 @@
 module.exports = {
   env: {
-    browser: true,
-    es2021: true,
+      browser: true,
+      es2021: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier'
-  ],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+      ecmaFeatures: {
+          jsx: true
+      },
+      ecmaVersion: "latest",
+      sourceType: "module"
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    'react/prop-types': 0,
-    'react/react-in-jsx-scope': 0,
-  },
-};
+  plugins: ["react"],
+  rules: ["react/react-in-jsx-scope"]
+}
