2e1dfa9a215200d21e1a31be7d0a98dce1f5fe9e
Roman <ramon1997.n8@gmail.com>
2022-05-22 13:47:53 +0300

    NewForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,16 @@
-import React, { useEffect, useState } from 'react';
 import './App.css';
+import Clothes from './components/Clothes';
+
 
 export default function App() {
-  const [name, setName] = useState()
-  const [discription, setDiscription] = useState()
-  const [price, setPrice] = useState()
-  const [type, setType] = useState([])
-
-  const [useChecked, setChecked] = useState(!'checked')
-
-  function whichIsTrue(target) {
-    useChecked === !'checked' ? setChecked('checked') : setChecked(!'checked')
-      if(useChecked === 'checked'){
-        setType(target)
-      }else{
-         setType('')
-     }
-  }
-
-  function show() {
-    console.log(
-      ` name: ${name};
-  discription: ${discription};
-  price: ${price};
-  type: ${type}
-      `
-    )
-  }
-
   return (
-    <div className='center'>
-      <div>
-        <input value={name} placeholder='name'
-          onChange={(e) => {
-            setName(e.target.value)
-          }}
-          style={{
-            width:'200px',
-            height:'20px',
-            marginTop: "50px"
-          }}
-        />
-      </div>
-      <div>
-        <input value={discription} placeholder='discription'
-          onChange={(e) => {
-            setDiscription(e.target.value)
-          }}
-          style={{
-            width:'200px',
-            height:'20px',
-            marginTop: "10px"
-          }}
-        />
-      </div>
-      <div>
-      <select className='center' onChange={(e) => {
-            setPrice(e.target.value)
-          }}
-          style={{
-            width:'210px',
-            height:'25px',
-            marginTop: "10px"
-          }}
-      >
-      <option value='price'>price</option>
-      <option value='100'>100</option>
-      <option value='200'>200</option>
-      <option value='300'>300</option>
-      <option value='400'>400</option>
-      <option value='500'>500</option>
-      </select>
+    
+    <>
+      <div className="blockabout">
+        <h2 className="blockabout_h2">Форма для заполнения:</h2>
+        <Clothes />
       </div>
 
-        <div style={{margin: 10}}>
-          <label>
-            <input type='checkbox' checked={useChecked} value='tshirt' onChange={(e) => {
-              whichIsTrue(e.target.value)
-            }}/>
-          tshirt</label>
-
-          <label>
-            <input type='checkbox' checked={useChecked} value='jeans' onChange={(e) => {
-              whichIsTrue(e.target.value)
-            }}/>
-          jeans</label>
-
-          <label>
-            <input type='checkbox' checked={useChecked} value='sweatshirt' onChange={(e) => {
-              whichIsTrue(e.target.value)
-            }}/>
-          sweatshirt</label>
-        </div>
-
-        <button type='submit' onClick={() => {show(name, discription, price, type)}} style={{
-            width:'200px',
-            height:'30px',
-            marginTop: "15px",
-            fontSize: '12px',
-            background: "#B0C4DE"
-          }}>create</button>
-    </div>
-  )
+    </>
+  );
 }
